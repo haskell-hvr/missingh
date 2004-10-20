f11929cb78a3f21200ca068b8ceab44922a3045f
John Goerzen <jgoerzen@complete.org>
2004-10-20 08:15:17 +0100

    Added WASHMail 0.3.6 files
    Keywords: 
    
    
    (jgoerzen@complete.org--projects/missingh--head--1.0--patch-77)

---
+++ libsrc/MissingH/WASHMail/EmailConfig.hs
@@ -0,0 +1,13 @@
+module EmailConfig where
+
+tmpDir, varDir, emailTmpDir, sendmailProgram :: String
+
+-- |temporary storage
+tmpDir = "/tmp/"
+-- |persistent, mutable storage
+varDir = "/tmp/"
+
+-- |temporary email files
+emailTmpDir 	= tmpDir
+-- |path of sendmail program
+sendmailProgram	= "/usr/sbin/sendmail"
