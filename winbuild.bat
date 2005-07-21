e1a687dec8273042261dabaceb8bb481c365175c
John Goerzen <jgoerzen@complete.org>
2005-07-21 22:04:35 +0100

    Added Windows build scripts

---
+++ winbuild.bat
@@ -0,0 +1,4 @@
+ghc -package Cabal Setup.lhs -o setup.exe
+setup configure
+setup build
+
---
+++ wintest.bat
@@ -0,0 +1,3 @@
+cd testsrc
+ghc --make -package mtl -package HUnit -fallow-overlapping-instances -fallow-undecidable-instances -fglasgow-exts -cpp -o runtests.exe -i..\dist\build:.. runtests.hs
+
