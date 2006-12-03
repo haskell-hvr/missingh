1bb27f676b176412b97960d27f554ef82130dd89
John Goerzen <jgoerzen@complete.org>
2006-12-03 06:45:01 +0100

    Updated

--- renameit.sh
+++ renameit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 OLDNAME="$1"
 NEWNAME="$2"
 
@@ -12,6 +14,10 @@ mod2destdir () {
         dirname "src/`mod2fn "$1"`"
 }
 
+sedname () {
+        echo "$1" | sed 's/\./\\./g' 
+}
+
 OLDFN=$(mod2fn "${OLDNAME}")
 NEWFN=$(mod2fn "${NEWNAME}")
 NEWDIR=$(mod2destdir "${NEWNAME}")
@@ -20,4 +26,22 @@ echo "New module name:     ${NEWNAME}"
 echo "Old module file:     ${OLDFN}"
 echo "New module file:     ${NEWFN}"
 echo "New module dir:      ${NEWDIR}"
+
+mkdir -p ${NEWDIR}
+darcs mv ${OLDFN} ${NEWFN}
+darcs add --recursive src
+
+for FILE in \
+        MissingH.cabal \
+        `find src -name "*.hs"` \
+        `find src -name "*.lhs"` \
+        `find MissingH -name "*.hs"` \
+        `find MissingH -name "*.lhs"` \
+        `find testsrc -name "*.hs"` 
+do
+        sed -i "s,`sedname ${OLDNAME}`,${NEWNAME},g" $FILE
+done
+
+darcs record -m "Renamed ${OLDNAME} to ${NEWNAME}"
+           
 
