3187b846a81b071e1cec773b16cda9509d11b7d0
John Goerzen <jgoerzen@complete.org>
2006-12-07 05:32:29 +0100

    Added hslogger to test scripts

--- wintest.bat
+++ wintest.bat
@@ -1,5 +1,5 @@
 cd testsrc
-ghc --make -package FilePath -package mtl -package HUnit -fallow-overlapping-instances -fallow-undecidable-instances -fglasgow-exts -cpp -o runtests.exe -i..\dist\build:..\src runtests.hs
+ghc --make -package hslogger -package FilePath -package mtl -package HUnit -fallow-overlapping-instances -fallow-undecidable-instances -fglasgow-exts -cpp -o runtests.exe -i..\dist\build:..\src runtests.hs
 cd ..
 testsrc\runtests
 
