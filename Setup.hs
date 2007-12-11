b7ffe166901d1730dfe54b5df267f6d2a68e45ad
Duncan Coutts <duncan@haskell.org>
2007-12-11 22:52:13 +0100

    Make it build with ghc-6.6 and 6.8 using configurations
    requires one slightly less than pleasent hack because Patternsignatures
    is not recognised by ghc-6.6 or the cabal that comes with 6.8.1, only
    by the latest cabal that comes with 6.8.2.
    Drop GHC-Options: -O2 as Cabal adds optimizations by default but also
    lets the user configure with --disable-optimization, so there is no longer
    any need to force optimisations on all the time and it prevents users
    configuring without optimisations for quick builds.

--- Setup.hs
+++ Setup.hs
@@ -1,22 +1,5 @@
 #!/usr/bin/env runhugs
 
 import Distribution.Simple
-import Distribution.PackageDescription
-import Distribution.Simple.LocalBuildInfo
-import Distribution.Version
-import System.Info
-import Data.Maybe
-import System.Cmd
 
-missingHooks = defaultUserHooks {confHook = customConfHook}
-
-customConfHook descrip flags =
-    let mydescrip = case System.Info.os of
-                      "mingw32" -> descrip
-                      _ -> descrip {buildDepends = 
-                                        (Dependency "unix" AnyVersion) :
-                                        buildDepends descrip}
-    in (confHook defaultUserHooks) mydescrip flags
-
-main = defaultMainWithHooks missingHooks
-
+main = defaultMain
