a2667700d715ec22de77bf39a231cd75ea93a0ab
Aistis Raulinaitis <sheganinans@gmail.com>
2014-05-15 04:29:11 -0700

    TSV parsing

---
+++ src/Data/SeperatingValues/SeperatingValues.hs
@@ -0,0 +1,52 @@
+{- |
+   Module     : Data.SeperatingValues.SeperatingValues
+   Copyright  : Copyright (C) 2005-2011 John Goerzen
+   License    : BSD3
+
+   Maintainer : John Goerzen <jgoerzen@complete.org>
+   Stability  : provisional
+   Portability: portable
+
+Misc/Helper Haskell Parsec parsers for any(X)-separated values (XSV) files.
+
+Written by: Aistis Raulinaitis, sheganinans@gmail.com
+-}
+
+module Data.SeperatingValues.SeperatingValues(eol, cellOfX, quotedchar, lineOfX, genXsvFile) where
+
+import Text.ParserCombinators.Parsec
+import Data.List (intersperse)
+
+eol :: forall st. GenParser Char st String
+eol = (try $ string "\n\r") <|> (try $ string "\r\n") <|> string "\n" <|>
+      string "\r" <?> "End of line"
+
+cellOfX :: Char -> GenParser Char st String
+cellOfX x = quotedcell <|> many (noneOf ([x] ++ "\n\r"))
+
+quotedchar :: GenParser Char st Char
+quotedchar = noneOf "\""
+             <|> (try $ do string "\"\""
+                           return '"'
+                 )
+quotedcell :: CharParser st String
+quotedcell = do char '"'
+                content <- many quotedchar
+                char '"'
+                return content
+
+lineOfX :: Char -> GenParser Char st [String]
+lineOfX x = sepBy (cellOfX x) (char x)
+
+genXsvFile :: String -> [[String]] -> String
+genXsvFile x inp =
+    unlines . map xsvline $ inp
+    where xsvline :: [String] -> String
+          xsvline l = concat . intersperse x . map xsvcells $ l
+          xsvcells :: String -> String
+          xsvcells "" = ""
+          xsvcells c = '"' : convcell c ++ "\""
+          convcell :: String -> String
+          convcell c = concatMap convchar c
+          convchar '"' = "\"\""
+          convchar x = [x]
--- src/Data/TSV.hs
+++ src/Data/TSV.hs
@@ -1,13 +1,5 @@
-{- arch-tag: CSV utilities
-Copyright (c) 2005-2011 John Goerzen <jgoerzen@complete.org>
-
-All rights reserved.
-
-For license and copyright information, see the file LICENSE
--}
-
 {- |
-   Module     : Data.CSV
+   Module     : Data.TSV
    Copyright  : Copyright (C) 2005-2011 John Goerzen
    License    : BSD3
 
@@ -15,32 +7,33 @@ For license and copyright information, see the file LICENSE
    Stability  : provisional
    Portability: portable
 
-Haskell Parsec parsers for comma-separated value (CSV) files.
+Haskell Parsec parsers for tab-separated value (TSV) files.
 
-Written by John Goerzen, jgoerzen\@complete.org
+Written by: Aistis Raulinaitis, sheganinans@gmail.com
 -}
+module Data.TSV where
 
-module Data.CSV (csvFile, genCsvFile) where
 import Text.ParserCombinators.Parsec
 import Data.SeperatingValues.SeperatingValues
 
 cell :: GenParser Char st String
-cell = cellOfX ','
+cell = cellOfX '\t'
 
 line :: GenParser Char st [String]
-line = lineOfX ','
+line = lineOfX '\t'
 
-{- | Parse a Comma-Separated Value (CSV) file.  The return value is a list of
+{- | Parse a Tab-Separated Value (TSV) file.  The return value is a list of
 lines; each line is a list of cells; and each cell is a String.
 
-Please note that CSV files may have a different number of cells on each line.
-Also, it is impossible to distinguish a CSV line that has a call with no data
-from a CSV line that has no cells.
+Please note that TSV files may have a different number of cells on each line.
+Also, it is impossible to distinguish a TSV line that has a call with no data
+from a TSV line that has no cells.
 
 Here are some examples:
 
 >Input (literal strings)          Parses As (Haskell String syntax)
 >-------------------------------- ---------------------------------
+
 >1,2,3                            [["1", "2", "3"]]
 >
 >l1                               [["l1"], ["l2"]]
@@ -55,23 +48,23 @@ Here are some examples:
 To parse a String, you might use:
 
 >import Text.ParserCombinators.Parsec
->import Data.String.CSV
+>import Data.String.TSV
 >....
->parse csvFile "" mystring
+>parse tsvFile "" mystring
 
 To parse a file, you might instead use:
 
->do result <- parseFromFile csvFile "/path/to/file"
+>do result <- parseFromFile tsvFile "/path/to/file"
 
 Please note that the result of parsing will be of type
 (Either ParseError [[String]]).  A Left result indicates an error.
 For more details, see the Parsec information.
 -}
 
-csvFile :: CharParser st [[String]]
-csvFile = endBy line eol
+tsvFile :: CharParser st [[String]]
+tsvFile = endBy line eol
 
-{- | Generate CSV data for a file.  The resulting string can be
+{- | Generate TSV data for a file.  The resulting string can be
 written out to disk directly. -}
-genCsvFile :: [[String]] -> String
-genCsvFile inp = genXsvFile "," inp
+genTsvFile :: [[String]] -> String
+genTsvFile inp = genXsvFile "\t" inp
