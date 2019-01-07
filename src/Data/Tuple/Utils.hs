df9ee883e2231beebd165b9cef0c4446282991a1
Wisut Hantanong <wizzup@users.noreply.github.com>
2019-01-07 15:28:26 +0700

    fix typo : 'lists' should be 'tuples'

--- src/Data/Tuple/Utils.hs
+++ src/Data/Tuple/Utils.hs
@@ -15,7 +15,7 @@ For license and copyright information, see the file LICENSE
    Stability  : provisional
    Portability: portable
 
-This module provides various helpful utilities for dealing with lists.
+This module provides various helpful utilities for dealing with tuples.
 
 Written by Neil Mitchell, <http://www.cs.york.ac.uk/~ndm/>
 -}
